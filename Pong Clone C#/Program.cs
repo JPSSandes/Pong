587d0e2bbb5bde26ef3f1396439450a3d150be02
JPSSandes <161637706+JPSSandes@users.noreply.github.com>
2024-10-01 20:50:49 -0300

    Pong Clone C#
    First commit of the pong clone made with C# and Raylib

---
+++ Pong Clone C#/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Pong
+{
+    class Program
+    {
+        static int windowWidth = 800;
+        static int windowHeight = 480;
+
+        public static void Main(string[] args)
+        {
+            Raylib.InitWindow(windowWidth, windowHeight, "Pong Clone C#: Remastered");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
